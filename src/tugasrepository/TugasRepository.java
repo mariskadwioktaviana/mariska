ff3d550112b859aea91675625153bd69b43de752
MYP <sword.arch@yahoo.co.id>
2014-10-08 13:53:25 +0700

    commit

---
+++ src/tugasrepository/TugasRepository.java
@@ -0,0 +1,15 @@
+package tugasrepository;
+
+public class TugasRepository {
+
+    public static void main(String[] args) {
+        System.out.println("_______TEKNOKRAT_______");
+        System.out.println("Kampusnya Sang Juara!!!");
+        String nama = "Mariska Dwi Oktaviana";
+        String kelas = "SI 13 E";
+        String npm = "13311374";
+        System.out.println("Nama :"+nama +"\nKelas:"+kelas +"\nNPM  :"+npm);
+
+    }
+    
+}
